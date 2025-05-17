699adfce5e9fb908cca255ac90dcf31dce9bdfd8
Asura <bankaikinowork@gmail.com>
2025-05-17 21:38:30 +0300

    new format

--- client/web/src/components/MailApp.jsx
+++ client/web/src/components/MailApp.jsx
@@ -2,33 +2,48 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ModalWindow from "./ModalWindow";
 import EmailList from "./EmailList";
+import SendFormSection from "./SendFormSection";
+import EditFormSection from "./EditFormSection";
+
 export default function MailApp() {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const [author, setAuthor] = useState("");
   const [foundIn, setFoundIn] = useState("");
   const [dataInt, setDataInt] = useState("");
+  const [newEditingMessage, setEditingNewMessage] = useState("");
+  const [editingAuthor, setEditingAuthor] = useState("");
+  const [editingFoundIn, setEditingFoundIn] = useState("");
+  const [editingDataInt, setEditingDataInt] = useState("");
   const [loading, setLoading] = useState(false);
   const [selectedMessage, setSelectedMessage] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [deletingId, setDeletingId] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingId, setEditingId] = useState(null);
+
   useEffect(() => {
     fetchMessages();
-  }, [messages]);
+
+    const intervalId = setInterval(fetchMessages, 60000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   const fetchMessages = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/messages");
-      setMessages(response.data);
+      const response = await axios.get("http://localhost:5000/api/letters");
+      setMessages(response.data.content);
     } catch (error) {
-      console.error("Ошибка отправки:", error);
+      console.error("ошибка отправки:", error);
     }
   };
 
   const deleteMessage = async (id) => {
     setDeletingId(id);
     try {
-      await axios.delete(`http://localhost:5000/messages/${id}`);
+      await axios.delete(`http://localhost:5000/api/letters/${id}`);
+      fetchMessages();
     } catch (error) {
       console.error("ошибка удаления:", error);
     } finally {
@@ -36,19 +51,52 @@ export default function MailApp() {
       setDeletingId(null);
     }
   };
+
+  const editMessage = async (e) => {
+    e.preventDefault();
+    if (!newEditingMessage.trim() || !editingAuthor.trim()) return;
+
+    setLoading(true);
+    try {
+      console.log(newEditingMessage);
+      const response = await axios.put(
+        `http://localhost:5000/api/letters/${editingId}`,
+        {
+          body: newEditingMessage,
+          author: editingAuthor.trim(),
+          found_at: editingDataInt.trim(),
+          found_in: editingFoundIn.trim(),
+          // id2: "1",
+        }
+      );
+      fetchMessages();
+      setMessages([...messages, response.data]);
+      setEditingNewMessage("");
+      setEditingAuthor("");
+      setEditingDataInt("");
+      setEditingFoundIn("");
+      setEditingId("");
+    } catch (error) {
+      console.error("ошибка отправки:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const sendMessage = async (e) => {
     e.preventDefault();
     if (!newMessage.trim() || !author.trim()) return;
 
     setLoading(true);
     try {
-      const response = await axios.post("http://localhost:5000/messages", {
-        text: newMessage,
+      const response = await axios.post("http://localhost:5000/api/letters", {
+        body: newMessage,
         author: author.trim(),
-        dataInt: dataInt.trim(),
-        foundIn: foundIn.trim(),
+        found_at: dataInt.trim(),
+        found_in: foundIn.trim(),
         // id2: "1",
       });
+      fetchMessages();
       setMessages([...messages, response.data]);
       setNewMessage("");
       setAuthor("");
@@ -61,11 +109,8 @@ export default function MailApp() {
     }
   };
 
-  const openMessage = (message, er, ertxt) => {
+  const openMessage = (message) => {
     setSelectedMessage(message);
-    setIsntError(er);
-    setErrorText(ertxt);
-    console.log(er);
     setShowModal(true);
   };
 
@@ -82,93 +127,89 @@ export default function MailApp() {
         onDeleteEmail={deleteMessage}
         deletingId={deletingId}
       ></EmailList>
-
+      {/* w-full md:w-1/2 lg:w-3/4 xl:w-2/3 */}
       <div className="max-w-6xl mx-auto mt-6 bg-gradient-to-r from-gray-200 via-gray-350 to-gray-400 rounded-lg shadow-md p-6">
-        <h2 className="text-xl font-semibold mb-4">Добавить новое письмо</h2>
-        <form onSubmit={sendMessage} className="space-y-4" autocomplete="off">
-          <div className="flex space-x-4">
-            <div className="flex flex-col w-full">
-              <label
-                htmlFor="author"
-                className=" text-sm font-medium text-gray-700 mb-1"
-              >
-                Автор
-              </label>
-              <input
-                id="author"
-                type="text"
-                value={author}
-                onChange={(e) => setAuthor(e.target.value)}
-                placeholder="автор"
-                className="block px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                required
-              />
-            </div>
-            <div className="flex flex-col">
-              <label
-                htmlFor="найдено"
-                className="block text-sm font-medium text-gray-700 mb-1"
-              >
-                найдено
-              </label>
-              <input
-                id="author"
-                type="text"
-                value={foundIn}
-                onChange={(e) => setFoundIn(e.target.value)}
-                placeholder="найдено"
-                className="block px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                required
-              />
-            </div>
-            <div className="flex flex-col">
-              <label
-                htmlFor="дата"
-                className="block text-sm font-medium text-gray-700 mb-1"
-              >
-                дата
-              </label>
-              <input
-                id="author"
-                type="text"
-                value={dataInt}
-                onChange={(e) => setDataInt(e.target.value)}
-                placeholder="дата"
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                required
-              />
-            </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="письмо"
-              className="block text-sm font-medium text-gray-700 mb-1"
+        <button
+          onClick={() => setIsEditing(false)}
+          className={`py-1 px-1 mr-3 rounded-lg text-white font-medium text-2xl font-semibold ${
+            isEditing
+              ? "bg-gray-500 hover:bg-gray-700"
+              : "bg-gray-600 hover:bg-gray-700"
+          }`}
+        >
+          Добавить новое письмо
+        </button>{" "}
+        <button
+          onClick={() => setIsEditing(true)}
+          className={`py-1 px-1 rounded-lg text-white font-medium text-2xl font-semibold mb-4 ${
+            isEditing
+              ? "bg-gray-600 hover:bg-gray-700"
+              : "bg-gray-500 hover:bg-gray-700"
+          }`}
+        >
+          Редактировать письмо
+        </button>
+        {isEditing == false ? (
+          <>
+            {" "}
+            <form
+              onSubmit={sendMessage}
+              className="space-y-4"
+              autocomplete="off"
             >
-              Письмо
-            </label>
-            <textarea
-              id="message"
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-              placeholder="пишите тут"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              rows={4}
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            disabled={loading || !newMessage.trim() || !author.trim()}
-            className={`px-6 py-2 rounded-lg text-white font-medium ${
-              loading || !newMessage.trim() || !author.trim()
-                ? "bg-gray-300 cursor-not-allowed"
-                : "bg-gray-600 hover:bg-gray-700"
-            }`}
-          >
-            {loading ? "отправка..." : "отправить"}
-          </button>
-        </form>
+              <SendFormSection
+                author={author}
+                setAuthor={setAuthor}
+                foundIn={foundIn}
+                setFoundIn={setFoundIn}
+                dataInt={dataInt}
+                setDataInt={setDataInt}
+                newMessage={newMessage}
+                setNewMessage={setNewMessage}
+              ></SendFormSection>
+
+              <button
+                type="submit"
+                disabled={loading || !newMessage.trim() || !author.trim()}
+                className={`px-6 py-2 rounded-lg text-white font-medium ${
+                  loading || !newMessage.trim() || !author.trim()
+                    ? "bg-gray-300 cursor-not-allowed"
+                    : "bg-gray-600 hover:bg-gray-700"
+                }`}
+              >
+                {loading ? "отправка..." : "отправить"}
+              </button>
+            </form>
+          </>
+        ) : (
+          <form onSubmit={editMessage} className="space-y-4" autocomplete="off">
+            <EditFormSection
+              editingId={editingId}
+              setEditingId={setEditingId}
+              editingAuthor={editingAuthor}
+              editingDataInt={editingDataInt}
+              editingFoundIn={editingFoundIn}
+              newEditingMessage={newEditingMessage}
+              setEditingAuthor={setEditingAuthor}
+              setEditingDataInt={setEditingDataInt}
+              setEditingFoundIn={setEditingFoundIn}
+              setEditingNewMessage={setEditingNewMessage}
+            ></EditFormSection>
+            <button
+              type="submit"
+              disabled={
+                loading || !newEditingMessage.trim() || !editingAuthor.trim()
+              }
+              className={`px-6 py-2 rounded-lg text-white font-medium ${
+                loading || !newEditingMessage.trim() || !editingAuthor.trim()
+                  ? "bg-gray-300 cursor-not-allowed"
+                  : "bg-gray-600 hover:bg-gray-700"
+              }`}
+            >
+              {loading ? "отправка..." : "отправить"}
+            </button>
+          </form>
+        )}
       </div>
 
       {showModal && (
@@ -176,8 +217,8 @@ export default function MailApp() {
           onClose={() => setShowModal(false)}
           dataInt={selectedMessage.found_at}
           title={selectedMessage.author}
-          foundIn={selectedMessage.foundIn}
-          text={selectedMessage.text}
+          foundIn={selectedMessage.found_in}
+          text={selectedMessage.body}
           backgroundImage="https://static.vecteezy.com/system/resources/previews/032/048/239/non_2x/paper-vintage-background-recycle-brown-paper-crumpled-texture-ai-generated-free-photo.jpg"
         ></ModalWindow>
       )}
